57850b0d813145f489023cfa24b5c04a0fc3f4ac
anand pandit <atim4077@gmail.com>
2023-03-19 12:51:01 +0530

    ui

--- src/components/EditBar/Container.tsx
+++ src/components/EditBar/Container.tsx
@@ -13,10 +13,8 @@ export const Container = ({
   const toggle = () => setOpen(!open);
   return (
     <div className="bg-gray-200 py-[2px] rounded-md my-1 select-none ">
-      <h1
-        className="text-md my-2 text-center text-gray-700  cursor-pointer flex justify-between items-center px-4"
-      >
-        {title}
+      <h1 className="text-md my-2  text-gray-700 gap-2  cursor-pointer flex justify-between items-center px-4">
+        <div className="w-[150px]">{title}</div>
         {all && (
           <>
             {elType == "input" && (
@@ -44,7 +42,11 @@ export const Container = ({
             )}
           </>
         )}
-        {open ? <FaChevronDown onClick={toggle} /> : <FaChevronRight onClick={toggle} />}
+        {open ? (
+          <FaChevronDown onClick={toggle} />
+        ) : (
+          <FaChevronRight onClick={toggle} />
+        )}
       </h1>
       {open && <div className="px-2">{children}</div>}
     </div>
