48621abd03a76cddf6caffb3e0e35ca7cc5c059d
anand pandit <atim4077@gmail.com>
2023-03-26 19:22:37 +0530

    app states added fully

--- src/state/app.slice.ts
+++ src/state/app.slice.ts
@@ -7,7 +7,19 @@ export interface AppState {
     hasFocus: boolean;
     ui: UIType;
     activeUI: ItemType | null;
+    platform: PlatformType;
+    hasFrameWork: boolean;
+    framework: FrameworkType;
+    libraries: string[];
+    uiLibrary: string;
+    screens?: {
+        [key: string]: UIType;
+    }
 }
+
+export type PlatformType = "web" | "android" | "desktop" | "ios" | "macos" | "win"
+export type FrameworkType = "react" | "react-native" | "flutter" | "vue" | "angular" | "svelte" | "none"
+export type UILibrary = "material-ui" | "react-native-paper" | 'native-base' | 'react-native-elements' | 'react-native-ui-kitten' | 'chakra-ui' | 'tailwind-css' | 'bootstrap' | 'semantic-ui' | 'ant-design' | 'none'
 
 const initialState: AppState = {
     activeElement: null,
@@ -21,7 +33,13 @@ const initialState: AppState = {
             },
         },
     ],
-    activeUI: null
+    activeUI: null,
+    platform: "android",
+    hasFrameWork: false,
+    framework: "react-native",
+    libraries: [],
+    uiLibrary: "none",
+    screens: {}
 };
 
 export const appSlice = createSlice({
@@ -37,11 +55,29 @@ export const appSlice = createSlice({
         },
         setActiveUI: (state, action) => {
             state.activeUI = action.payload;
+        },
+        setPlatform: (state, action) => {
+            state.platform = action.payload;
+        },
+        setFramework: (state, action) => {
+            state.framework = action.payload;
+        },
+        setUILibrary: (state, action) => {
+            state.uiLibrary = action.payload;
+        },
+        setHasFrameWork: (state, action) => {
+            state.hasFrameWork = action.payload;
+        },
+        setLibraries: (state, action) => {
+            state.libraries = action.payload;
+        },
+        setScreens: (state, action) => {
+            state.screens = action.payload;
         }
     },
 });
 
 
-export const { setActiveElement, setUI,setActiveUI } = appSlice.actions;
+export const { setActiveElement, setUI, setActiveUI, setFramework, setPlatform, setUILibrary, setHasFrameWork, setLibraries, setScreens } = appSlice.actions;
 
 export default appSlice.reducer;
