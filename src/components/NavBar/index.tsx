ba10dcdd177cc08e384b22ebc91f0442af01860b
anand pandit <atim4077@gmail.com>
2023-05-20 23:41:07 +0530

    rename project added

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,14 +2,28 @@ import React from "react";
 import CopyAsCode from "./CopyAsCode";
 import SaveChanges from "./SaveChanges";
 import DownloadCode from "./DownloadCode";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../state/store";
+import { updateName } from "../../state/app.slice";
 
 const NavBar = ({ screenCode }: NavBarProps) => {
+  const name = useSelector((state: RootState) => state.app.name);
+  const dispatch = useDispatch();
+  const handleProjectName = (e: any) => {
+    dispatch(updateName(e.target.value));
+  };
   return (
     <nav className=" w-full bg-white px-2 flex  flex-col justify-center  ">
       <div className="flex justify-center text-gray-700 font-bold py-2">
-        REACT NATIVE GUI
+        REACT NATIVE GUI{" "}
+        <input
+          className="ml-2 font-light inline-block"
+          type="text"
+          value={name}
+          onChange={handleProjectName}
+        />
       </div>
-      <div className="flex flex-row justify-end mb-[10px]   px-2 items-center py-2  bg-gray-100  gap-4 rounded-xl">
+      <div className="flex flex-row justify-end mb-[10px]    px-2 items-center py-2  bg-gray-100  gap-4 rounded-xl">
         <DownloadCode />
         <SaveChanges />
         <CopyAsCode screenCode={JSON.parse(JSON.stringify(screenCode))} />
